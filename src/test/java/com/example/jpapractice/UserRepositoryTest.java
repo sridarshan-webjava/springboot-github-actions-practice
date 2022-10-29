c38a97b88693ee057d799547db56c73036e80a4b
Sridarshan Balaji <sridarshan1997@gmail.com>
2022-10-29 17:47:49 +0530

    Remove unwanted imports and run workflow for main branch

--- src/test/java/com/example/jpapractice/UserRepositoryTest.java
+++ src/test/java/com/example/jpapractice/UserRepositoryTest.java
@@ -4,15 +4,11 @@ import com.example.jpapractice.User.User;
 import com.example.jpapractice.User.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import javax.transaction.Transactional;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @DataJpaTest
 public class UserRepositoryTest {
